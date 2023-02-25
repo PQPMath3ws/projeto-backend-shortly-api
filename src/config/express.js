46cd5dddcb676bcdf89fcc45e7e32ca01017407a
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-02-25 11:01:59 -0300

    Configuração de estágio inicial do express

---
+++ src/config/express.js
@@ -0,0 +1,39 @@
+import * as dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+
+import { closePostgresPoolAndClient } from "./database.js";
+
+import AllRoutes from "../routes/AllRoutes.js";
+
+dotenv.config();
+
+const app = express();
+let server = null;
+
+async function onShutDownServer() {
+    if (server) {
+        closePostgresPoolAndClient((error) => {
+            if (error) console.log(error);
+            server.close(() => {
+                process.exit(0);
+            });
+        });
+    }
+}
+
+async function initializeServer() {
+    if (!server) {
+        app.use(cors());
+        app.use(express.json());
+
+        app.use(AllRoutes);
+
+        server = app.listen(process.env.PORT);
+    }
+
+    process.on("SIGTERM", async () => await onShutDownServer());
+    process.on("SIGINT", async () => await onShutDownServer());
+}
+
+export default initializeServer;
